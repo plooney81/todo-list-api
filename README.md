20ddc7e1b827a099f279f0862f0c35a576906a48
Pete Looney <petelooney81@gmail.com>
2020-10-02 13:49:29 -0500

    refactored and added readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# TO DO LIST API <!-- omit in toc -->
+
+- [About this repository](#about-this-repository)
+- [Lessons Learned](#lessons-learned)
+- [Technology Used](#technology-used)
+  
+## About this repository
+* This repo was created for two reasons:
+  
+    1 To test knowledge of how to create a REST API
+
+    2 To tie together our newly created REST API and previous front end knowledge to create a static site.
+
+## Lessons Learned
+* How to create a REST API
+* How to use Express Middleware packages (specifically the body-parser package)
+* How to use AXIOS GET as well as other Front End techniques to load specific data on to a page
+* How to use AXIOS POST to add data from the user to our server
+
+## Technology Used
+* Node.js
+* Express
+* jQuery
+* AXIOS
+* Boostrap
--- public/script.js
+++ public/script.js
@@ -15,32 +15,29 @@ async function getList(){
         });
 
         $putListHere.append(`
-        <h1 class="text-justify">List: </h1>
-        <ul class="list-group">${newItem.join('')}</ul>
-        `);
-        // $putListHere.append(`
-        //     <h1 class="text-justify">List: </h1>
-        //     <ul class="list-group">${newItem.join('')}</ul>
+            <h1 class="text-justify">List: </h1>
+            <ul class="list-group">${newItem.join('')}</ul>
 
-        //     <form class="mt-3">
-        //         <div class="form-group">
-        //             <label for="newItemId">Item Id</label>
-        //             <input type="number" class="form-control" id="newItemId" placeholder="5">
-        //         </div>
-        //         <div class="form-group">
-        //             <label for="newAction">Action</label>
-        //             <input type="text" class="form-control" id="newAction" placeholder="Make more money">
-        //         </div>
-        //         <button type="submit" class="btn btn-primary" id="addNewItem">Add</button>
-        //     </form>
-        // `);
+            <form class="mt-3">
+                <div class="form-group">
+                    <label for="newItemId">Item Id</label>
+                    <input type="number" class="form-control" id="newItemId" placeholder="5">
+                </div>
+                <div class="form-group">
+                    <label for="newAction">Action</label>
+                    <input type="text" class="form-control" id="newAction" placeholder="Make more money">
+                </div>
+                <button type="submit" class="btn btn-primary" id="addNewItem">Add</button>
+            </form>
+        `);
     })
 }
 
 $(document).ready(()=>{getList();})
 
-$submitButton.click((e)=>{
+$(document).click((e)=>{
     e.preventDefault();
+   if(e.target.id === 'addNewItem'){
     console.log('HELLO');
     const $newItemId = $('#newItemId');
     const $newAction = $('#newAction');
@@ -55,4 +52,5 @@ $submitButton.click((e)=>{
                 $newAction.val('');
             });
     }
+   }
 })
